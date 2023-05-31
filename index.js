dfe8d958f332df9f055338871a42ea9bf5b71a1a
Guilherme da Costa Silva <guilhermedacsilva@gmail.com>
2023-05-31 19:10:03 -0300

    arrumando

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const T20 = {
 
 window.addEventListener('message', async ({ data }) => {
     if (data.type === 't20-scripts-loaded') {
+        // checks if roll20 editor is loaded, then load my modules
         await checkTimeout(() => $('#editor').length)
         Object.entries(T20.modules).forEach(([key, module]) => {
             module.onLoad($('body'))
