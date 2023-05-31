6191ff227af0b6ed9e5dbd9ca3d95d2dd8068cd2
Guilherme da Costa Silva <guilhermedacsilva@gmail.com>
2023-05-31 20:44:20 -0300

    funcionou

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 'use strict'
 
+function loadJsOnRoll20(path) {
+    let element = document.createElement('script')
+    element.src = chrome.runtime.getURL('js/' + path)
+    element.type = 'text/javascript'
+    let node = (document.head || document.documentElement)
+    node.appendChild(element)
+}
+
 function loadCssOnRoll20(path) {
     let element = document.createElement('link')
-    element.href = chrome.runtime.getURL(path)
+    element.href = chrome.runtime.getURL('css/'+path)
     element.rel = 'stylesheet'
     element.type = 'text/css'
     let node = (document.head || document.documentElement)
     node.appendChild(element)
 }
 
-function loadJsOnRoll20(path) {
-    let element = document.createElement('script')
-    element.src = chrome.runtime.getURL(path)
-    element.type = 'text/javascript'
-    let node = (document.head || document.documentElement)
-    node.appendChild(element)
+function loadCssOnRoll20CharSheet(charId) {
+    const iframe = $(`iframe[name="iframe_${charId}"]`).contents()
+    iframe.find('head').append($(`<link href="${chrome.runtime.getURL('css/app.css')}" rel="stylesheet">`))
 }
 
 function loadData(path) {
-    fetch(chrome.runtime.getURL(path))
+    fetch(chrome.runtime.getURL('data/' + path))
         .then(response => response.json())
         .then(json => {
             const name = path.split('/').pop().split('.')[0]
@@ -30,34 +35,21 @@ $(document).ready(function () {
 
     console.log('Roll20-T20 init')
 
-    loadJsOnRoll20('js/utils.js')
-    loadJsOnRoll20('js/app.js')
-    loadJsOnRoll20('js/listeners.js')
-    loadJsOnRoll20('js/habilities.js')
+    loadJsOnRoll20('utils.js')
+    loadJsOnRoll20('app.js')
+    loadJsOnRoll20('listeners.js')
+    loadJsOnRoll20('habilities.js')
 
-    loadData('data/spells.json')
-    loadData('data/habilities.json')
+    loadData('spells.json')
+    loadData('habilities.json')
+
+    $(window).on('message', ({ originalEvent: { data } }) => {
+        if (data.type === 'loaded') {
+            loadCssOnRoll20CharSheet(data.characterId)
+        }
+    })
 
     /*
-    loadJsOnRoll20('modules/_api.js')
-    loadJsOnRoll20('modules/_utils.js')
-    loadJsOnRoll20('modules/attacks-and-equipments.js')
-    loadJsOnRoll20('modules/builder.js')
-    loadJsOnRoll20('modules/handouts.js')
-    loadJsOnRoll20('modules/macros.js')
-    //  loadJsOnRoll20('modules/meet.js')
-    loadJsOnRoll20('modules/powers-and-abilities.js')
-    loadJsOnRoll20('modules/spells.js')
-    loadJsOnRoll20('modules/threats.js')
-    //  loadJsOnRoll20('modules/tokens.js')
-    //  loadJsOnRoll20('modules/tracker.js')
-
-    //loadCssOnRoll20('sheet.css')
-
-    loadData('data/equipments.json')
-    loadData('data/powers.json')
-    loadData('data/rules.json')
-    loadData('data/spells.json')
 
     setTimeout(() => window.postMessage({ type: 't20-scripts-loaded' }, '*'), 100)
 
@@ -71,14 +63,7 @@ $(document).ready(function () {
             window.postMessage({ type: 'FROM_CONTENT', text: JSON.stringify(book) }, '*')
         })
 
-    $(window).on('message', ({ originalEvent: { data } }) => {
-        if (data.type === 'loaded') {
-            const iframe = $(`iframe[name="iframe_${data.characterId}"]`).contents()
-            iframe.find('head').append($(`<link href="${chrome.runtime.getURL('sheet.css')}" rel="stylesheet">`))
-            console.log('Window loaded')
-            console.log(data)
 
-        }
-    })
     */
+
 })
