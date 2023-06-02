be8cebb413fb3a3b9f1bd96ebf9ef354c58181e1
Guilherme da Costa Silva <guilhermedacsilva@gmail.com>
2023-06-02 13:51:03 -0300

    magias

--- js/app.js
+++ js/app.js
@@ -7,65 +7,109 @@ const T20 = {
 }
 
 T20.modules.habilities = {
-    initSheet: ($iframe, characterId) => {
-        const div = $iframe.find('.sheet-powers-and-abilities')
-        const click = function () {
-            T20.utils.showDialogHabilities($iframe, characterId)
+    initSheet: ($iframe) => {
+        const openDialog = function () {
+            T20.utils.showDialogHabilities($iframe)
         }
-        div.find('.repcontrol_add:eq(0)')
-            .after($('<button rel="abilities" class="btn repcontrol_more">T20</button>').click(click))
-        div.find('.repcontrol_add:eq(1)')
-            .after($('<button rel="powers" class="btn repcontrol_more">T20</button>').click(click))
+        $iframe.find('.sheet-powers-and-abilities')
+            .find('.repcontrol_add')
+            .after($('<button class="btn repcontrol_more">T20</button>').click(openDialog))
+    }
+}
+
+T20.modules.spells = {
+    initSheet: ($iframe) => {
+        const openDialog = function () {
+            T20.utils.showDialogSpells($iframe)
+        }
+        $iframe.find('.sheet-spells')
+            .find('.repcontrol_add')
+            .after($('<button class="btn repcontrol_more">T20</button>').click(openDialog))
     }
 }
 
 T20.utils = {
-    /*
-    closeContextMenu() {
-        T20.d20.token_editor.closeContextMenu()
+    cortarTexto(nTd, sData, oData, iRow, iCol) {
+        if (oData.descricao.length > 150) {
+            $(nTd).html(oData.descricao.substring(0, 150) + '...')
+        }
     },
-    getCurrentPage() {
-        return T20.d20.Campaign.activePage()
-    },
-    getCurrentLayer() {
-        return window.currentEditingLayer
-    },
-    getCanvasMousePos() {
-        return [...T20.d20.engine.mousePos]
-    },
-    */
+
     prepararLinkPoder(nTd, sData, oData, iRow, iCol, $iframe) {
-        $(nTd).html(`<span class="nome-click">${oData[2]}</span>`);
+        $(nTd).html(`<span class="nome-click">${oData[2]}</span>`)
         nTd.addEventListener("click", () => {
             T20.utils.fichaInserirPoder(oData[0], $iframe)
-        });
+        })
+    },
+
+    prepararLinkMagia(nTd, sData, oData, iRow, iCol, $iframe) {
+        $(nTd).html(`<span class="nome-click">${oData[2]}</span>`)
+        nTd.addEventListener("click", () => {
+            T20.utils.fichaInserirMagia(oData, $iframe)
+        })
     },
 
     fichaInserirPoder(habilityIndex, $iframe) {
         let poder = T20.db.habilities[habilityIndex]
         let colunaDireita = $('#poderColunaD').prop("checked")
 
-        let poderes = $iframe.find('[class="sheet-container-reapeater"]');
-        let htmlName = 'ability';
+        let poderes = $iframe.find('[class="sheet-container-reapeater"]')
+        let htmlName = 'ability'
         if (colunaDireita) {
-            poderes = poderes[1];
-            htmlName = 'power';
+            poderes = poderes[1]
+            htmlName = 'power'
         } else {
-            poderes = poderes[0];
+            poderes = poderes[0]
         }
 
-        poderes.querySelector('[class="btn repcontrol_add"]').click();
-        let itens = poderes.querySelectorAll('[class="repitem"]');
-        var novoItem = itens[itens.length - 1];
+        poderes.querySelector('[class="btn repcontrol_add"]').click()
+        let itens = poderes.querySelectorAll('[class="repitem"]')
+        var novoItem = itens[itens.length - 1]
 
-        novoItem.querySelector(`[name="attr_name${htmlName}"]`).value = poder[2];
-        novoItem.querySelector(`[name="attr_${htmlName}description"]`).value = poder[3];
+        novoItem.querySelector(`[name="attr_name${htmlName}"]`).value = poder[2]
+        novoItem.querySelector(`[name="attr_${htmlName}description"]`).value = poder[3]
 
         setTimeout(function () {
-            novoItem.querySelector(`[name="attr_name${htmlName}"]`).dispatchEvent(new Event('blur'));
-            novoItem.querySelector(`[name="attr_${htmlName}description"]`).dispatchEvent(new Event('blur'));
-        }, 10);
+            novoItem.querySelector(`[name="attr_name${htmlName}"]`).dispatchEvent(new Event('blur'))
+            novoItem.querySelector(`[name="attr_${htmlName}description"]`).dispatchEvent(new Event('blur'))
+        }, 10)
     },
+
+    fichaInserirMagia(magia, $iframe) {
+        let $spellGroup = $iframe.find('.repeating_spells' + magia.circulo).parent()
+        $spellGroup.find('.repcontrol_add').click()
+        let $novoItem = $spellGroup.find('.repitem').last()
+
+        $novoItem.find('[name="attr_namespell"]').val(magia.nome)
+        $novoItem.find('[name="attr_spellalcance"]').val(magia.alcance)
+        $novoItem.find('[name="attr_spellalvoarea"]').val(magia.alvo)
+        $novoItem.find('[name="attr_spelltipo"]').val(magia.escola)
+        $novoItem.find('[name="attr_spellduracao"]').val(magia.duracao)
+        $novoItem.find('[name="attr_spellexecucao"]').val(magia.execucao)
+        $novoItem.find('[name="attr_spelldescription"]').val(magia.descricao.replaceAll('<br>', '\n'))
+        if ('resistencia' in magia) {
+            $novoItem.find('[name="attr_spellresistencia"]').val(magia.resistencia)
+        }
+
+        setTimeout(function () {
+            let inputs = [
+                "attr_namespell",
+                "attr_spellalcance",
+                "attr_spellalvoarea",
+                "attr_spelltipo",
+                "attr_spellduracao",
+                "attr_spellexecucao",
+                "attr_spelldescription",
+                "attr_spellresistencia"
+            ]
+            inputs.forEach(inputName => {
+                //novoItem.find(`[name="${inputName}"]`).dispatchEvent(new Event('blur'))
+                novoItem.find(`[name="${inputName}"]`).trigger('blur')
+            })
+        }, 10)
+
+    },
+
 
     showDialogHabilities($iframe) {
         const dialog = $(`
@@ -107,9 +151,8 @@ T20.utils = {
             close: () => dialog.remove()
         })
         const modal = dialog.closest('.ui-dialog')
-        const titleBar = modal.find('.ui-dialog-titlebar')
         const content = modal.find('.ui-dialog-content')
-        const tableHabilities = content.find('#roll20-t20-table-habilities').DataTable({
+        content.find('#roll20-t20-table-habilities').DataTable({
             data: T20.db.habilities,
             columns: [
                 { title: 'Tipo', data: 1, orderData: [0, 1] },
@@ -135,93 +178,65 @@ T20.utils = {
                 },
             },
         })
-        /*
-        if (extraOptions.padding) {
-            content.css({ padding: extraOptions.padding })
-        }
-        if (!title) {
-            titleBar.css({ padding: 8 })
-            titleBar.find('.ui-dialog-titlebar-close').css({ top: 7, right: 0, zIndex: 10 })
-            titleBar.find('.ui-dialog-title').remove()
-        }
-        form.on('submit', async e => {
-            e.preventDefault()
-            const formValues = {}
-            form.find(':input').each(function () {
-                const name = $(this).attr('name')
-                if (name) formValues[name] = $(this).val()
-            })
-            try {
-                callback && await callback(formValues)
-            } catch (err) {
-                console.error(err)
-                return err.message && alert(err.message)
-            }
-            dialog.dialog('close')
-        })
-        setTimeout(() => {
-            content.find(':input:eq(0)').focus()
-        }, 1500)
-        */
-        return dialog
     },
-    showSelectDialog(title, options, callback) {
-        const input = $('<input type="text" name="value" value="" placeholder="Digite para buscar...">')
-        input.on('keydown', e => {
-            if (e.keyCode === 13) setTimeout(() => { form.submit() }, 200)
-        })
-        this.showDialog(title, input, ({ value }) => {
-            if (options[value] && callback) callback(options[value])
-        })
-        setTimeout(() => {
-            input.autocomplete({ source: Object.keys(options) })
-            input.focus()
-        }, 200)
-    },
-    showDialog(title, inputsEl, callback, extraOptions = {}) {
-        const dialog = $(`<div class="${extraOptions.class || ''}" title="${title}"><form>`)
-        const form = dialog.find('form').append(inputsEl)
-            .append('<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">')
+
+
+    showDialogSpells($iframe) {
+        const dialog = $(`
+        <div class="roll20-t20-conteudo">
+            <table id="roll20-t20-table-spells" class="table table-sm">
+              <thead>
+                <tr>
+                  <th>C</th>
+                  <th>Escola</th>
+                  <th>Nome</th>
+                  <th class="coluna-400">Resumo</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+        </div>`)
         dialog.dialog({
+            title: 'Listagem de magias',
             autoOpen: true,
-            buttons: callback ? {
-                Confirmar: () => form.submit(),
-                Cancelar: () => dialog.dialog('close')
-            } : {
-                Ok: () => dialog.dialog('close')
-            },
-            close: () => dialog.remove(),
-            ...extraOptions
+            height: 500,
+            width: 900,
+            close: () => dialog.remove()
         })
         const modal = dialog.closest('.ui-dialog')
-        const titleBar = modal.find('.ui-dialog-titlebar')
         const content = modal.find('.ui-dialog-content')
-        if (extraOptions.padding) {
-            content.css({ padding: extraOptions.padding })
-        }
-        if (!title) {
-            titleBar.css({ padding: 8 })
-            titleBar.find('.ui-dialog-titlebar-close').css({ top: 7, right: 0, zIndex: 10 })
-            titleBar.find('.ui-dialog-title').remove()
-        }
-        form.on('submit', async e => {
-            e.preventDefault()
-            const formValues = {}
-            form.find(':input').each(function () {
-                const name = $(this).attr('name')
-                if (name) formValues[name] = $(this).val()
-            })
-            try {
-                callback && await callback(formValues)
-            } catch (err) {
-                console.error(err)
-                return err.message && alert(err.message)
-            }
-            dialog.dialog('close')
+        content.find('#roll20-t20-table-spells').DataTable({
+            data: T20.db.spells,
+            columns: [
+                { title: 'C', data: 'circulo', orderData: [0, 1, 2] },
+                { title: 'Escola', data: 'escola', orderData: [1, 0, 2] },
+                {
+                    title: 'Nome',
+                    data: 'nome',
+                    orderData: 2,
+                    fnCreatedCell: (nTd, sData, oData, iRow, iCol) => {
+                        T20.utils.prepararLinkMagia(nTd, sData, oData, iRow, iCol, $iframe)
+                    }
+                },
+                {
+                    title: 'Descrição',
+                    data: 'descricao',
+                    orderData: 3,
+                    fnCreatedCell: T20.utils.cortarTexto
+                },
+            ],
+            language: {
+                search: 'Pesquisar',
+                lengthMenu: 'Mostrar _MENU_ itens',
+                info: 'Exibindo do _START_ ao _END_ de um total de _TOTAL_ itens',
+                paginate: {
+                    first: 'Primeira',
+                    last: 'Última',
+                    next: 'Próxima',
+                    previous: 'Anterior'
+                },
+            },
         })
-        setTimeout(() => {
-            form.find(':input:eq(0)').focus()
-        }, 1500)
-        return dialog
-    }
+    },
 }
